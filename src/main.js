fc13a789225e9d5e3725f1967397537aaecd714f
Filixitu <2582903926@qq.com>
2025-05-29 15:29:32 +0800

    完成计算直方图功能

--- src/main.js
+++ src/main.js
@@ -3,10 +3,15 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+
 // 引入element-ui
 import Element from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 Vue.use(Element)
+
+import * as echarts from 'echarts'
+Vue.prototype.$echarts = echarts
+
 
 Vue.config.productionTip = false
 
--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,12 @@ module.exports = defineConfig({
   transpileDependencies: true,
 
   devServer: {
+    //    proxy: {
+    //   '/api': {
+    //     target: 'http://47.109.197.221:8080',
+    //     changeOrigin: true
+    //   }
+    // },
     port: 7070
   }
 })
